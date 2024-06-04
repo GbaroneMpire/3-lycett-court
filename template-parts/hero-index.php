3b09e0e7afd30cac9349c999eabd682cd3684602
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-04 14:11:10 +0200

    markup

--- template-parts/hero-index.php
+++ template-parts/hero-index.php
@@ -1,10 +1,41 @@
+<?php 
+
+  $hero = get_field('lct-hero');
+  $bg = $hero['background_image']['ID'];
+  $button = $hero['button'];
+?>
+
 <section class="lct-hero">
   <div class="container">
 
     <div class="lct-hero__content lct-section">
 
-        
+      <?php if($bg): ?>
+        <figure class="lct-hero__background lct-background-cover">
+          <?php wp_get_attachment_image( $bg, 'lct-banner', null, array(
+            'class' => 'lct-object-fit'
+          )); ?>
+        </figure>
+      <?php endif; ?>
 
+      <aside class="lct-hero__text-content">
+
+          <span class="lct-hero__subtext display-block">
+            <?= $hero['subtext']; ?>
+          </span>
+
+          <span>
+            <?= $hero['headline']; ?>
+          </span>
+
+          <?php if($button): 
+            $link_target = $button['target'] ? $button['target'] : '_self';
+          ?>
+              <a href="<?= esc_url($button['url']) ?>" target="<?= esc_attr($link_target) ?>"><?= esc_html($button['title']) ?></a>
+          <?php endif; ?>
+
+      </aside>
+      
     </div>
 
   </div>

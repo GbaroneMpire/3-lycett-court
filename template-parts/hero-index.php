ff5d47b80c9d59bf42a22f784b2e3d99a5958549
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-04 19:31:09 +0200

    update

--- template-parts/hero-index.php
+++ template-parts/hero-index.php
@@ -7,37 +7,44 @@
 ?>
 
 <section class="lct-hero">
+
+  <link rel="stylesheet" href="././theme/hero.css">
+
   <div class="container">
+    <div class="row">
 
-    <div class="lct-hero__content lct-section">
-
-      <?php if($bg): ?>
-        <figure class="lct-hero__background lct-background-cover">
-          <?= wp_get_attachment_image( $bg, 'lct-banner', null, array(
-            'class' => 'lct-object-fit'
-          )); ?>
-        </figure>
-      <?php endif; ?>
-
-      <aside class="lct-hero__text-content">
-
-          <span class="lct-hero__subtext display-block">
-            <?= $fields['subtext']; ?>
-          </span>
-
-          <span>
-            <?= $fields['headline']; ?>
-          </span>
-
-          <?php if($button): 
-            $link_target = $button['target'] ? $button['target'] : '_self';
-          ?>
-              <a href="<?= esc_url($button['url']) ?>" target="<?= esc_attr($link_target) ?>"><?= esc_html($button['title']) ?></a>
+      <div class="col-lg">
+        <div class="lct-hero__content lct-section position-relative d-flex align-items-center">
+    
+          <?php if($bg): ?>
+            <figure class="lct-hero__background lct-background-cover">
+              <?= wp_get_attachment_image( $bg, 'lct-banner', null, array(
+                'class' => 'lct-object-fit bottom'
+              )); ?>
+            </figure>
           <?php endif; ?>
+    
+          <aside class="lct-hero__text-content position-relative d-flex flex-column">
+    
+              <span class="lct-hero__subtext display-block">
+                <?= $fields['subtext']; ?>
+              </span>
+    
+              <span>
+                <?= $fields['headline']; ?>
+              </span>
+    
+              <?php if($button): 
+                $link_target = $button['target'] ? $button['target'] : '_self';
+              ?>
+                  <a href="<?= esc_url($button['url']) ?>" target="<?= esc_attr($link_target) ?>"><?= esc_html($button['title']) ?></a>
+              <?php endif; ?>
+    
+          </aside>
+          
+        </div>
+      </div>
 
-      </aside>
-      
     </div>
-
   </div>
 </section>

56ac503238b1c47ce16c800ba12f13f2a79dd9f3
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-04 14:20:47 +0200

    front page template

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -6,7 +6,23 @@
 ?>
 
 <main id="main">
-  <?php get_template_part('/template-parts/index', 'hero'); ?>
+  <?php 
+    if ( have_rows('lct_index_sections') ):
+
+      while( have_rows('lct_index_sections')) : the_row();
+
+        $layout = get_row_layout();
+
+        switch ($layout) {
+          case 'hero':
+            get_template_part('/template-parts/hero', 'index');
+            break;
+        }
+
+      endwhile;
+      
+    endif;
+  ?>
 </main><!-- /.container -->
 
 <?php 

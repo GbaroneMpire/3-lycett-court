aaa39ac653aafd18d66f6cd4af1de1bda049870d
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-06 00:32:57 +0200

    update

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -12,32 +12,14 @@
       while( have_rows('lct_index_sections')) : the_row();
 
         $layout = get_row_layout();
-
-        if ($layout == 'hero') {
-
-          $fields = get_sub_field('hero')['lct_hero'];
+        $fields = get_sub_field($layout);
 
           if ($fields) {
-            get_template_part('/template-parts/hero', 'index', array(
+            get_template_part("/template-parts/{$layout}", null, array(
               'fields' => $fields
             ));
           }
 
-        }
-
-        if ($layout == 'side-by-side') {
-
-
-          $field_group = get_sub_field('side-by-side')['lct_side_by_side'];
-
-          if ($field_group) {
-            get_template_part( '/template-parts/side-by-side', null, array(
-              'field_group' => $field_group
-            ));
-          }
-   
-  
-        }
 
       endwhile;
 

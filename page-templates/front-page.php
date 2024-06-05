c821696b3f87dd460f230cb87ca2a607d374d336
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-06 01:12:32 +0200

    update

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -14,8 +14,6 @@
         $layout = get_row_layout();
         $fields = get_sub_field($layout)["lct_{$layout}"];
 
-        var_dump($fields);
-
         if ($fields) {
           get_template_part("/template-parts/{$layout}", null, array(
             'fields' => $fields

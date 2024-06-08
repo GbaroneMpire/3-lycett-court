98bef6055fd37e5cb130a8ba8d0f6a20f28d3df0
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-08 20:18:59 +0200

    gallery section template

--- template-parts/gallery-section.php
+++ template-parts/gallery-section.php
@@ -8,5 +8,28 @@
 
   extract($args);
  
-  lct_test($fields);
 ?>
+
+<section class="lct-gallery-section lct-section">
+
+  <section class="container-md lct-smaller lct-title-section">
+    <div class="row">
+      <div class="col">
+        <div class="lct-grid">
+            <h2><?= $fields['headline'] ?></h2>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="container-md background-dark gallery-section">
+    <div class="row">
+      <div class="col">
+        <?php get_template_part('template-parts/gallery', null, array(
+          'fields' => $fields['lct_gallery']
+        )); ?>
+      </div>
+    </div>
+  </section>
+
+</section>

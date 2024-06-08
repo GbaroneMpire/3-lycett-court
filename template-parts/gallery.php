269a9ce68aca69b1234134994eaf947d25f069f9
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-08 20:23:46 +0200

    test

--- template-parts/gallery.php
+++ template-parts/gallery.php
@@ -16,7 +16,7 @@
 
   <?php if($filters): ?>
     <section class="lct-filters">
-      <?php foreach($filters as $filters): ?>
+      <?php foreach($filters as $filter): ?>
         <button data-filter=".<?= $filter['value'] ?>"><?= $filter['label'] ?></button>
       <?php endforeach; ?>        
     </section>
@@ -26,7 +26,7 @@
     <section class="lct-gallery__description"><?= $description ?></section>
   <?php endif; ?>
 
-  <?php if($gallery): ?>
+  <?php if($gallery_images): ?>
     <section class="lct-gallery">
       <?php foreach($gallery_images as $image): ?>
           <figure class="lct-gallery-image">

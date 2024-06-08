ae0e953df07e35bf84f3da0b6473e3f0819d9a42
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-08 20:53:57 +0200

    gallery section styles and templating

--- template-parts/gallery-section.php
+++ template-parts/gallery-section.php
@@ -6,24 +6,32 @@
 
 <?php extract($args); ?>
 
-<section class="lct-gallery-section lct-section">
+<section class="lct-gallery-section lct-section lct-bg-accent">
 
   <section class="container-md lct-smaller lct-title-section">
     <div class="row">
       <div class="col">
-        <div class="lct-grid">
+        <div class="lct-grid grid-4">
             <h2><?= $fields['heading'] ?></h2>
         </div>
       </div>
     </div>
   </section>
 
-  <section class="container-md background-dark gallery-section">
+  <section class="gallery-section container-md bg-dark text-white">
     <div class="row">
       <div class="col">
-        <?php get_template_part('/template-parts/gallery', null, array(
-          'fields' => $fields['lct_gallery']
-        )); ?>
+
+        <div class="container-md lct-smaller px-5">
+          <div class="row">
+            <div class="col">
+              <?php get_template_part('/template-parts/gallery', null, array(
+                'fields' => $fields['lct_gallery']
+              )); ?>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </section>

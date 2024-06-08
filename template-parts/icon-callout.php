aa46f29b7624b1602299bc81eee9792ba7c43448
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-08 17:51:32 +0200

    update icon callout template

--- template-parts/icon-callout.php
+++ template-parts/icon-callout.php
@@ -10,9 +10,18 @@
   $icon_handle = $fields['icon_handle'];
   $main_text = $fields['main_text'];
   $sub_text = $fields['sub_text'];
+  $column_direction = 'flex-column';
+  $border_class = 'border-bottom';
+  $gap_class = 'lct-gap-1';
+
+  if (isset($is_icon_callout_section)) {
+    $column_direction = 'flex-column-reverse';
+    $border_class = 'border-0';
+    $gap_class = 'lct-gap-2';
+  }
 
 ?>
-<div class="lct-callout d-flex <?= $fields['layout']['value'] ?> lct-gap-2 align-items-center pb-4 border-bottom">
+<div class="lct-callout d-flex <?= $fields['layout'] ?> lct-gap-3 align-items-center pb-4 <?= $border_class ?> <?= $fields['text_align'] ?>">
 
   <?php if($icon_handle): ?>
     <figure class="lct-callout__icon">
@@ -20,7 +29,7 @@
     </figure>
   <?php endif; ?>
 
-  <div class="lct-callout__text d-flex flex-column lct-gap-1">
+  <div class="lct-callout__text d-flex <?= $column_direction ?> <?= $gap_class ?>">
     <?php if($sub_text): ?>
       <span class="lct-callout__sub_text"><?= $sub_text ?></span>
     <?php endif; ?>

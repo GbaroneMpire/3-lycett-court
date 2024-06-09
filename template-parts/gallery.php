69994bb04f70c186d505b1baee6cfa2bdaee71de
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-09 11:11:50 +0200

    update

--- template-parts/gallery.php
+++ template-parts/gallery.php
@@ -32,8 +32,15 @@
     <section class="lct-gallery position-relative">
       <div class="lct-inner d-flex flex-column">
         <div class="lct-gallery-track lct-gap-3 lct-grid grid-2 w-100">
-          <?php foreach($gallery_images as $image): ?>
-              <figure class="lct-gallery-image">
+          <?php foreach($gallery_images as $image): 
+              $image_data = lct_get_image_data($image['ID']);
+              $image_description = $image_data['description'];
+              $image_class = '';
+
+              $image_class .= (!empty($image_description)) ? ' ' . $image_description : '';
+            
+            ?>
+              <figure class="lct-gallery-image<?= $image_class ?>">
                 <?= wp_get_attachment_image( $image['ID'], null, null, array(
                   'class' => 'lct-object-fit'
                 )); ?>
